0ad0dd32f6fde365da4a7c03b96247305cc9947d
Rada Berar <rada.berar@ohanacode-dev.com>
2020-10-04 19:14:14 +0200

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # myip
 Service to keep track of devices IP address.
+
+## Purpose of this service
+Sometimes I build various devices that I need to access remotely. 
+For this I need to know the IP address of a device. 
+This is a service to provide a simple way to keep track of devices IP address 
+and it is free for anyone to use. It is like a Dynamic DNS service, 
+but you can host this anywhere, even within your local network. 
+Keep in mind that if you do host this service on your own network, 
+the IP address reported will not be external, but a local one. 
+To get an external address this must be hosted on an external server.
+
+I do not employ a domain name registrar, nor do I provide a hosting service, 
+so I can not provide you with an domain that your devices would use directly, 
+but just a text based record of an updated IP address.
+
+## How it works
+To keep things simple, there is no login, no registration. 
+Every device using this service must have it's own unique id. 
+We will refer to this as [device_id]. You can generate the id yourself. 
+Choose any set of characters. I am using a private ssh key, 
+but you can use anything else you are sure is unique.
+
+On the device whose IP address you wish to keep track of make an http get request 
+(or manually using your web browser) to:
+
+    https://myip.ohanacode-dev.com/set/[device_id]
+
+On your computer, from which you wish to find devices IP address, 
+open a web browser and go to:
+
+    https://myip.ohanacode-dev.com/get/[device_id]
+
+This will return just your devices IP address. If your device provides a web server 
+and you need a page to bookmark, you can use:
+
+    https://myip.ohanacode-dev.com/redirect/[device_id]
+
+This will provide a page redirecting to your devices IP address.
+
+# Running the service
+
+This is a Python Flask based application, so install Flask and run the py file 
+using Python3. Take a look at the end of the python file for hint on selecting 
+parameters for local or external server hosting. 
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,71 @@
-from flask import Flask, render_template
- 
+from flask import Flask, render_template, request, redirect, make_response, url_for
+import sqlite3
+from contextlib import closing
+
+SERV_NAME = "myip.ohanacode-dev.com"
+DB_NAME = "ip_log"
+
 application = Flask(__name__)
-# application.config['SERVER_NAME'] = "myip.ohanacode-dev.com"
-application.config['SESSION_COOKIE_DOMAIN'] = "myip.ohanacode-dev.com"
+# application.config['SERVER_NAME'] = SERV_NAME
+# application.config['SESSION_COOKIE_DOMAIN'] = SERV_NAME
+
+
+def create_db():
+    with closing(sqlite3.connect(DB_NAME)) as connection:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute("CREATE TABLE IF NOT EXISTS {} (id TEXT, ip TEXT)".format(DB_NAME))
+
+
+def set_ip(dev_id, ip_addr):
+    result = None
+
+    if ip_addr is not None and dev_id is not None:
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                try:
+                    rows = cursor.execute('SELECT ip FROM {} WHERE id = "{}"'.format(DB_NAME, dev_id)).fetchone()
+
+                    if rows is not None and len(rows) > 0:
+                        # Already there
+                        cursor.execute("UPDATE {} SET ip = ? WHERE id = ?".format(DB_NAME), (ip_addr, dev_id))
+                    else:
+                        # Insert new
+                        query = "INSERT INTO {} VALUES ('{}', '{}')".format(DB_NAME, dev_id, ip_addr)
+                        cursor.execute(query)
+
+                    connection.commit()
+                    result = 'OK'
+                except Exception as e:
+                    if str(e).startswith('no such table'):
+                        # Create the table
+                        create_db()
+                        result = "Table created. Try again."
+                    else:
+                        print(e)
+
+    return result
+
+
+def get_ip(dev_id):
+    result = None
+
+    if dev_id is not None:
+        with closing(sqlite3.connect(DB_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                try:
+                    row = cursor.execute('SELECT ip FROM {} WHERE id="{}" '.format(DB_NAME, dev_id)).fetchone()
+                    if len(row) > 0:
+                        result = row[0]
+                except Exception as e:
+                    if str(e).startswith('no such table'):
+                        # Create the table
+                        create_db()
+                        result = "Table created. Try again."
+                    else:
+                        print(e)
+
+    print("RETURN:", result)
+    return result
 
 
 @application.route('/')
@@ -10,6 +73,46 @@ def home():
     return render_template('index.html')
 
 
+@application.route('/set', methods=['GET'])
+def set_ip_blank():
+    msg = "<h1>ERROR 406</h1> " \
+          "<p>This page is not intended to be used like this. You should select a device ID " \
+          "(Any random, unique, HTML escaped set of characters and " \
+          "add it to the GET request sent from your device like: <br>" \
+          "&nbsp &nbsp <b>{}/[your_device_id]</b><br><br>Then to get the devices IP address, go to: <br>" \
+          "&nbsp &nbsp <b>{}/get/[your_device_id]</b><br><br></p>" \
+          "<h2>NOTE</h2> " \
+          "<p>If this service is on an external hosting service, for improved security, it is recommended to use " \
+          "https protocol so append <b>'https://'</b> to the url examples above.</p>".format(SERV_NAME, SERV_NAME)
+    return msg, 406
+
+
+@application.route('/set/<id>', methods=['GET'])
+def set_ip_dev(id):
+    response = make_response(set_ip(id, request.remote_addr), 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+@application.route('/get', methods=['GET'])
+def get_ip_blank():
+    response = make_response(request.remote_addr, 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+@application.route('/get/<id>', methods=['GET'])
+def get_ip_dev(id):
+    result = get_ip(id)
+
+    response = make_response("{}".format(result), 200)
+    response.mimetype = "text/plain"
+    return response
+
+
 if __name__ == '__main__':
+    # uncomment this if you are hosting the application on an external hosting provider
     # application.run()
+
+    # Use this instead to run on your local computer
     application.run('0.0.0.0', 8888, threaded=True, debug=False)
