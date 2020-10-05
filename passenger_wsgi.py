999237ca003be81d3ed32f08ea3e24dba3450365
Rada Berar <rada.berar@rt-rk.com>
2020-10-05 13:52:25 +0200

    Add remove old data feature

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, make_response, url_for
 import sqlite3
 from contextlib import closing
+from time import time
 
 SERV_NAME = "myip.ohanacode-dev.com"
 DB_NAME = "ip_log"
+SPAM_PROTECTION_TIMEOUT = 10
 
 application = Flask(__name__)
 # application.config['SERVER_NAME'] = SERV_NAME
@@ -13,28 +15,46 @@ application = Flask(__name__)
 def create_db():
     with closing(sqlite3.connect(DB_NAME)) as connection:
         with closing(connection.cursor()) as cursor:
-            cursor.execute("CREATE TABLE IF NOT EXISTS {} (id TEXT, ip TEXT)".format(DB_NAME))
+            cursor.execute("CREATE TABLE IF NOT EXISTS {} (id TEXT, ip TEXT, time INT)".format(DB_NAME))
 
 
 def set_ip(dev_id, ip_addr):
-    result = None
+    result = 'None'
 
     if ip_addr is not None and dev_id is not None:
         with closing(sqlite3.connect(DB_NAME)) as connection:
             with closing(connection.cursor()) as cursor:
                 try:
-                    rows = cursor.execute('SELECT ip FROM {} WHERE id = "{}"'.format(DB_NAME, dev_id)).fetchone()
+                    id_row = cursor.execute('SELECT * FROM {} WHERE id = "{}"'.format(DB_NAME, dev_id)).fetchone()
+                    ip_row = cursor.execute('SELECT * FROM {} WHERE ip = "{}"'.format(DB_NAME, ip_addr)).fetchone()
+                    timestamp = int(time())
 
-                    if rows is not None and len(rows) > 0:
-                        # Already there
-                        cursor.execute("UPDATE {} SET ip = ? WHERE id = ?".format(DB_NAME), (ip_addr, dev_id))
+                    if ip_row is not None and len(ip_row) > 0:
+                        # We already have records from this IP
+                        ip_timestamp = ip_row[2]
+                        timeout = SPAM_PROTECTION_TIMEOUT - (timestamp - ip_timestamp)
+
+                        if timeout < 0:
+                            # Only allow updating every 10 seconds or more.
+                            # Delete all records with this IP
+                            cursor.execute("DELETE FROM {} WHERE ip = ?".format(DB_NAME), (ip_addr,))
+                            # Delete all records with this device ID
+                            cursor.execute("DELETE FROM {} WHERE id = ?".format(DB_NAME), (dev_id,))
+
+                            # Insert new data
+                            query = "INSERT INTO {} VALUES ('{}', '{}', {})".format(DB_NAME, dev_id, ip_addr, timestamp)
+                            cursor.execute(query)
+                            connection.commit()
+                            result = 'OK'
+                        else:
+                            result = 'ERROR: Spam protection. Please wait {}s'.format(timeout)
                     else:
-                        # Insert new
-                        query = "INSERT INTO {} VALUES ('{}', '{}')".format(DB_NAME, dev_id, ip_addr)
+                        # Insert new data
+                        query = "INSERT INTO {} VALUES ('{}', '{}', {})".format(DB_NAME, dev_id, ip_addr, timestamp)
                         cursor.execute(query)
+                        connection.commit()
+                        result = 'OK'
 
-                    connection.commit()
-                    result = 'OK'
                 except Exception as e:
                     if str(e).startswith('no such table'):
                         # Create the table
@@ -43,6 +63,10 @@ def set_ip(dev_id, ip_addr):
                     else:
                         print(e)
 
+                # Cleanup old data
+                old_timestamp = time() - (60 * 60 * 48)
+                cursor.execute("DELETE FROM {} WHERE time < ?".format(DB_NAME), (old_timestamp,))
+                connection.commit()
     return result
 
 
@@ -53,9 +77,10 @@ def get_ip(dev_id):
         with closing(sqlite3.connect(DB_NAME)) as connection:
             with closing(connection.cursor()) as cursor:
                 try:
-                    row = cursor.execute('SELECT ip FROM {} WHERE id="{}" '.format(DB_NAME, dev_id)).fetchone()
-                    if len(row) > 0:
+                    row = cursor.execute('SELECT ip FROM {} WHERE id = ?'.format(DB_NAME), (dev_id,)).fetchone()
+                    if row is not None and len(row) > 0:
                         result = row[0]
+
                 except Exception as e:
                     if str(e).startswith('no such table'):
                         # Create the table
@@ -64,7 +89,6 @@ def get_ip(dev_id):
                     else:
                         print(e)
 
-    print("RETURN:", result)
     return result
 
 
